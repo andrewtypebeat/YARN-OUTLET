48e198478f318591c50e361ef9282d9d01de7296
Andrew White <91177473+andrewtypebeat@users.noreply.github.com>
2026-04-09 23:53:31 -0600

    1

--- yarn-outlet/Scripts/debug.gd
+++ yarn-outlet/Scripts/debug.gd
@@ -1,6 +1,8 @@
 extends Node2D
 @onready var enemyDebug = preload("res://Scenes/Objects/Enemy/test_enemy.tscn")
 
+var loop_ = 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -10,7 +12,10 @@ func _process(delta):
 	pass
 
 func _on_enemy_timer_timeout():
-	var en_debug = enemyDebug.instantiate()
+	var cur_enemy = enemyDebug.instantiate()
 	var random_y = randi_range(30,200)
-	en_debug.position = Vector2(340, random_y)
-	add_child(en_debug)
+	cur_enemy.position = Vector2(340, random_y)
+	add_child(cur_enemy)
+
+#AAHHH YA SE COMO EN PROCESS HAY UN MATCH PARA UNA VARIABLE DE SPAWN Y A LO
+#LARGO DEL NIVEL HAY NODOS AREA2D QUE ACTIVAN ESAS SEÑALES Y LAS DESACTIVAN INMEDIATAMENTE DESPUES
